393e5305a82183334b60157e0da4c0e495a798fa
Jan T. Sott <git@idleberg.com>
2022-08-21 01:08:22 +0200

    add spinner

--- bin/build.mjs
+++ bin/build.mjs
@@ -6,6 +6,7 @@ import { minify } from 'html-minifier-terser';
 import { promises as fs } from 'node:fs'; 
 import { render } from 'ejs';
 import hljs from 'highlight.js';
+import ora from 'ora';
 import path from 'node:path';
 import sqlite3 from 'sqlite3';
 
@@ -22,6 +23,7 @@ const htmlMinifyOptions = {
 };
 
 marked.setOptions({
+    renderer: new marked.Renderer(),
     baseUrl: path.relative(Dir.documents, path.resolve(Dir.documents, 'html')),
     highlight: code => hljs.highlight(code, {language: 'nsis'}).value
 });
@@ -34,24 +36,34 @@ await createIndex()
 async function initTable() {
     try {
         await fs.rm(Dir.docset, { force: true, recursive: true });
-        await fs.mkdir(Dir.resources, { recursive: true });
+        await fs.mkdir(Dir.documents, { recursive: true });
     } catch (e) {
-        console.warn('Could not create build directory');
+        console.warn(`Could not create build directory`);
     }
 
-    db = new sqlite3.Database(path.resolve(Dir.resources, 'docSet.dsidx'));
+    const spinner = ora('Initializing database').start();
+    
+    try {
+        db = new sqlite3.Database(path.resolve(Dir.resources, 'docSet.dsidx'));
+    
+        db.serialize(function() {
+            db.run("DROP TABLE IF EXISTS searchIndex;");
+            db.run("CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);");
+            db.run("CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);");
+        });
 
-    db.serialize(function() {
-        db.run("DROP TABLE IF EXISTS searchIndex;");
-        db.run("CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);");
-        db.run("CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);");
-    });
+        spinner.succeed();
+    } catch(err) {
+        spinner.fail(err.message);
+    }
 }
 
 async function createPages() {
     const docsPath = path.resolve(__dirname, 'node_modules/@nsis/docs/**/*.md');
     const filePaths = await globby([docsPath, '!**/*README.md']);
     const template = await fs.readFile(path.resolve(__dirname, 'src/templates/docset.ejs'), 'utf8');
+
+    const spinner = ora('Creating pages').start();
 
     filePaths.forEach(async filePath => {
         const relativeDocsPath = path.dirname(path.relative('node_modules/@nsis/docs/docs', filePath));
@@ -61,13 +73,14 @@ async function createPages() {
 
         const markdownContent = await fs.readFile(filePath, 'utf8');
         const htmlContent = marked.parse(markdownContent).replaceAll(/\.md/g, '.html');
-        const minifiedContent = await minify(render(template, {
+        const minifiedContent = (await minify(render(template, {
             version: version?.length ? `v${version}` : 'dev',
             ghLink: `https://github.com/NSIS-Dev/Documentation/edit/main/docs/${relativeDocsPath}/${fileName}.md`,    
             pageTitle: canonicalName,
             contents: htmlContent,
             relativeAssets: path.relative(outPath, Dir.documents)
-        }), htmlMinifyOptions);
+        }), htmlMinifyOptions))
+        .replaceAll('<pre><code>', '<pre><code class="hljs language-nsis">');
 
         try {
             await fs.mkdir(outPath, { recursive: true });
@@ -88,53 +101,74 @@ async function createPages() {
             db.run(`INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ('${getFile(filePath).name}', '${getType(filePath)}', '${relativePath}');`);
         });
     });
+
+    spinner.succeed();
 }
 
 async function createIndex() {
-    const template = await fs.readFile(path.resolve(__dirname, 'src/templates/index.ejs'), 'utf8');
+    const spinner = ora('Creating index page...');
 
-    const minifiedContent = await minify(render(template, {
-        version: version?.length ? `v${version}` : 'dev',
-        xmlFeed: encodeURIComponent('http://nsis-dev.github.io/Docset-Feed/NSIS.xml')
-    }), htmlMinifyOptions);
+    try {
+        const template = await fs.readFile(path.resolve(__dirname, 'src/templates/index.ejs'), 'utf8');
 
-    const outFile = path.resolve(
-        Dir.documents,
-        'index.html'
-    );
+        const minifiedContent = await minify(render(template, {
+            version: version?.length ? `v${version}` : 'dev',
+            xmlFeed: encodeURIComponent('http://nsis-dev.github.io/Docset-Feed/NSIS.xml')
+        }), htmlMinifyOptions);
 
-    await fs.writeFile(outFile, minifiedContent);
+        const outFile = path.resolve(
+            Dir.documents,
+            'index.html'
+        );
+
+        await fs.writeFile(outFile, minifiedContent);
+        spinner.succeed();
+    } catch(err) {
+        spinner.fail(err.message);
+    }
 }
 
 async function copyStaticFiles() {
-    const fontsDir = path.resolve(`${Dir.documents}/fonts`);
-    await fs.mkdir(fontsDir, { recursive: true });
+    const spinner = ora('Copying static files');
 
-    const imgDir = path.resolve(`${Dir.documents}/img`);
-    await fs.mkdir(imgDir, { recursive: true });
+    try {
+        const fontsDir = path.resolve(`${Dir.documents}/fonts`);
+        await fs.mkdir(fontsDir, { recursive: true });
 
-    await fs.copyFile(path.resolve(__dirname, 'src/static/icon.png'), `${Dir.docset}/icon.png`);
-    await fs.copyFile(path.resolve(__dirname, 'src/static/icon@2x.png'),  `${Dir.docset}/icon@2x.png`);
-    await fs.copyFile(path.resolve(__dirname, 'src/static/Info.plist'),  `${Dir.contents}/Info.plist`);
-    await fs.mkdir(fontsDir, { recursive: true });
-    await fs.copyFile(path.resolve(__dirname, 'node_modules/@nsis/logo/dist/Logo/outlines-light.svg'),  `${imgDir}/logo.svg`);
+        const imgDir = path.resolve(`${Dir.documents}/img`);
+        await fs.mkdir(imgDir, { recursive: true });
 
-    const fonts = [
-        'FiraMono-Regular.eot',
-        'FiraMono-Regular.ttf',
-        'FiraMono-Regular.woff2',
-        'FiraSans-Light.eot',
-        'FiraSans-Light.ttf',
-        'FiraSans-Light.woff2',
-        'FiraSans-Regular.eot',
-        'FiraSans-Regular.ttf',
-        'FiraSans-Regular.woff2'
-    ];
+        const cssDir = path.resolve(`${Dir.documents}/css`);
+        await fs.mkdir(cssDir, { recursive: true });
 
-    Promise.all(fonts.map(async font => {
-        const extension = path.extname(font).replace(/^\./, '');
-        await fs.copyFile(path.resolve(__dirname, `node_modules/@mozilla/fira/${extension}/${font}`), `${fontsDir}/${font}`);
-    }));    
+        await fs.copyFile(path.resolve(__dirname, 'src/css/theme.css'), `${Dir.documents}/css/theme.css`);
+        await fs.copyFile(path.resolve(__dirname, 'src/static/icon.png'), `${Dir.docset}/icon.png`);
+        await fs.copyFile(path.resolve(__dirname, 'src/static/icon@2x.png'),  `${Dir.docset}/icon@2x.png`);
+        await fs.copyFile(path.resolve(__dirname, 'src/static/Info.plist'),  `${Dir.contents}/Info.plist`);
+        await fs.mkdir(fontsDir, { recursive: true });
+        await fs.copyFile(path.resolve(__dirname, 'node_modules/@nsis/logo/dist/Logo/outlines-light.svg'),  `${imgDir}/logo.svg`);
+
+        const fonts = [
+            'FiraMono-Regular.eot',
+            'FiraMono-Regular.ttf',
+            'FiraMono-Regular.woff2',
+            'FiraSans-Light.eot',
+            'FiraSans-Light.ttf',
+            'FiraSans-Light.woff2',
+            'FiraSans-Regular.eot',
+            'FiraSans-Regular.ttf',
+            'FiraSans-Regular.woff2'
+        ];
+
+        Promise.all(fonts.map(async font => {
+            const extension = path.extname(font).replace(/^\./, '');
+            await fs.copyFile(path.resolve(__dirname, `node_modules/@mozilla/fira/${extension}/${font}`), `${fontsDir}/${font}`);
+        }));
+
+        spinner.succeed();
+    } catch(err) {
+        spinner.fail(err.message);
+    }
 }
 
 function getType(filePath) {
